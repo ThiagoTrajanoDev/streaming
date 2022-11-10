8ab7e6d503cc1b5a8a7080da19772cb94ac1289b
BoloSalgadoo <ttf1880@gmail.com>
2022-11-10 20:27:55 -0300

    alteracao no link google

--- js/main.js
+++ js/main.js
@@ -329,7 +329,7 @@ function sair(){
 }
 // FUNÇÃO PARA ABERTURA DE POP UP PARA LOGIN EM GOOGLE E FACE
 google.addEventListener("click", ()=>{
-    window.open("https://accounts.google.com/v3/signin/identifier?dsh=S185187890%3A1668034931963208&hl=pt-br&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=ARgdvAvsBflEhmR_yLeCVs6GZYHWBF0NNiC7EAPHmtAra8rFAsm-RcjvUmVbSDULdOBsh7RRcIUFHg","Fazer login com conta Google","toolbar=no,location=no,directories=no,status=no, menubar=no,scrollbars=yes,resizable=no,width=600, height=550,top=150,left=600")
+    window.open("https://accounts.google.com/v3/signin/identifier?dsh=S135106958%3A1667686411265880&authuser=0&continue=https%3A%2F%2Fwww.google.com%2Fsearch%3Fclient%3Dopera-gx%26q%3Dgoogle%2Blogin%26sourceid%3Dopera%26ie%3DUTF-8%26oe%3DUTF-8&ec=GAlAAQ&hl=pt-BR&flowName=GlifWebSignIn&flowEntry=AddSession","Fazer login com conta Google","toolbar=no,location=no,directories=no,status=no, menubar=no,scrollbars=yes,resizable=no,width=600, height=550,top=150,left=600")
 })
 face.addEventListener("click", ()=>{
     window.open("https://pt-br.facebook.com/","Fazer login com conta Google","toolbar=no,location=no,directories=no,status=no, menubar=no,scrollbars=yes,resizable=no,width=600, height=550,top=150,left=600")
